e21abc42507d3421f4c50a6f89bada18898361ab
Jean Brito <jeanfbrito@gmail.com>
2012-12-19 20:18:12 -0200

    First Commit˜

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImobiliUs::Application.config.secret_token = '71018e3876d2dc2b3bb72ec4025731bcc407672027664fd89ee5ba15cbb5f5e04810bb67275f841698b75928fa6ced5e651d8e13a575904071f8bc1798d557cd'
